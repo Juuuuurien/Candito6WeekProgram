bf3914df63f0aaa02174dd962aaa93b167a52216
Julien Lopez <julien24Lopez@gmail.com>
2020-07-19 15:35:35 -0700

    - Cleaned up WorkoutScreen.js of unused comment blocks

--- src/screens/WorkoutScreen/WorkoutScreen.js
+++ src/screens/WorkoutScreen/WorkoutScreen.js
@@ -1,119 +1,13 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-  Text,
-  Button,
-} from "react-native";
+import { View, Text, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { globalStyles, colors, headerStyles } from "../../styles/global";
-import AddWorkoutScreen from "./addWorkoutScreen/addWorkoutScreen";
-import WorkoutScreenContext from "./workoutScreenContext";
-import WorkoutCalendar from "./CalendarScreen/CalendarComponent";
-import SettingsModal from "./SettingsModal";
 import CalendarScreen from "./MainStackScreens/CalendarScreen";
 
 import SettingsStackScreen from "./ModalStackScreens/SettingsStackScreen";
 import NoWorkoutsScreen from "./ModalStackScreens/NoWorkoutsScreen";
 import AsyncStorage from "@react-native-community/async-storage";
 import { AppLoading } from "expo";
-
-// const Workout = () => {
-//   const [startDate, setStartDate] = useState(false);
-//   const [visibility, setVisibility] = useState(false);
-
-//   const WorkoutProvider = (props) => {
-//     return (
-//       <WorkoutScreenContext.Provider
-//         value={{
-//           setStartDate: setStartDate,
-//           setVisibility: setVisibility,
-//           visibility: visibility,
-//         }}
-//       >
-//         {props.children}
-//       </WorkoutScreenContext.Provider>
-//     );
-//   };
-
-//   if (!startDate) {
-//     return (
-//       <View style={{ flex: 1 }}>
-//         <WorkoutProvider>
-//           <AddWorkoutScreen />
-//         </WorkoutProvider>
-//       </View>
-//     );
-//   } else {
-//     return (
-//       <View style={{ flex: 1 }}>
-//         <WorkoutCalendar></WorkoutCalendar>
-//       </View>
-//     );
-//   }
-// };
-
-// const Stack = createStackNavigator();
-
-// const WorkoutScreen = () => {
-//   //get current date for header title.
-//   const moment = require("moment");
-//   const today = moment().format("dddd, MMMM Do YYYY");
-
-//   return (
-//     <Stack.Navigator
-//       initialRouteName={Workout}
-//       screenOptions={{
-//         title: today,
-//         headerShown: true,
-//         headerTitleAlign: "center",
-//         headerStyle: { ...globalStyles.headerBackgroundColor },
-//         headerTintColor: "#fff",
-//         headerTitleStyle: { ...globalStyles.headerTitleStyle },
-//         cardStyle: { backgroundColor: colors.darkerGray },
-//       }}
-//     >
-//       <Stack.Screen name="Workout" component={Workout} headerMode="float" />
-//       <Stack.Screen
-//         name="SettingsModal"
-//         component={SettingsModal}
-//         headerMode="float"
-//       />
-//     </Stack.Navigator>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   modalBackgroundView: {
-//     backgroundColor: "#5DB075",
-//     height: "100%",
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   modalView: {
-//     width: "90%",
-//     backgroundColor: "white",
-//     borderRadius: 20,
-//     padding: 35,
-//     shadowColor: "#000",
-//     shadowOffset: {
-//       width: 0,
-//       height: 2,
-//     },
-//     shadowOpacity: 0.25,
-//     elevation: 5,
-//   },
-//   buttonText: {
-//     fontSize: 24,
-//     fontWeight: "bold",
-//     color: "white",
-//   },
-//   addPrompt: {
-//     fontSize: 18,
-//   },
-// });
 
 const MainStack = createStackNavigator();
 const ModalStack = createStackNavigator();
